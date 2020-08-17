6b9cce6bc9a62d569b527db6bbf5c3175f9c5253
daoka <daoka.sweep@gmail.com>
2020-08-17 15:03:31 +0900

    feat: 🎸 added partial tx audit

--- src/service/AuditService.ts
+++ src/service/AuditService.ts
@@ -1,5 +1,5 @@
-import { RepositoryFactoryHttp, TransactionGroup, AggregateTransaction, TransferTransaction, UInt64, BlockInfo, Address } from "symbol-sdk";
-import { IAuditResult } from "@src/model";
+import { RepositoryFactoryHttp, TransactionGroup, AggregateTransaction, TransferTransaction, UInt64, BlockInfo, Address, MultisigAccountModificationTransaction } from "symbol-sdk";
+import { IAuditResult, IPartialTxAuditResult } from "@src/model";
 import { IApostilleTxMessage } from "../model/ApostilleTxMessage";
 import { HashFunctionCreator, DataView } from "../utils/hash";
 
@@ -16,6 +16,16 @@ export class AuditService {
     return result;
   }
 
+  public static async auditWithPartial(
+    data: DataView,
+    txHash: string,
+    apiEndpoint: string,
+  ) {
+    const service = new AuditService(data, txHash, apiEndpoint);
+    const result = await service.auditWithPartial();
+    return result;
+  }
+
   constructor(
     private readonly data: DataView,
     private readonly txHash: string,
@@ -26,24 +36,53 @@ export class AuditService {
 
   private async audit() {
     const coreTx = await this.getTransaction();
-    const ownerPublicAccount = coreTx.signer!;
+    const singerPublicAccount = coreTx.signer!;
     const parsedMessage = this.parseMessage(coreTx.message.payload);
     const hashFunction = HashFunctionCreator.createWithTypeStr(parsedMessage.hashingTypeStr);
     const hashedData = hashFunction.hashing(this.data);
-    const isValid = ownerPublicAccount.verifySignature(hashedData, parsedMessage.signedHash);
+    const isValid = singerPublicAccount.verifySignature(hashedData, parsedMessage.signedHash);
     if (isValid) {
-      const owner = coreTx.signer!.address;
+      const signer = coreTx.signer!.address;
       const apostilleAccount = coreTx.recipientAddress as Address;
       const timestamp = await this.getTimestamp(coreTx.transactionInfo!.height);
       const result: IAuditResult = {
         isValid: true,
-        owner,
+        signer,
         apostilleAccount,
         timestamp
       }
       return result;
     }
     const result: IAuditResult = { isValid: false };
+    return result;
+  }
+
+  private async auditWithPartial() {
+    const tx = await this.getTransactionFromPartial();
+    const coreTx = this.getCoreTx(tx);
+    const signerPublicAccount = coreTx.signer!;
+    const parsedMessage = this.parseMessage(coreTx.message.payload);
+    const hashFunction = HashFunctionCreator.createWithTypeStr(parsedMessage.hashingTypeStr);
+    const hashedData = hashFunction.hashing(this.data);
+    const isValid = signerPublicAccount.verifySignature(hashedData, parsedMessage.signedHash);
+    if (isValid) {
+      const signer = coreTx.signer!.address;
+      const apostilleAccount = coreTx.recipientAddress as Address;
+      const multisigTx = this.getMultisigTx(tx);
+      const additions = multisigTx.addressAdditions;
+      const cosignatories = this.getCosignatories(tx);
+      const result: IPartialTxAuditResult = {
+        isValid: true,
+        signer,
+        apostilleAccount,
+        multisigAdditional: additions,
+        cosignatories
+      };
+      return result;
+    }
+    const result: IPartialTxAuditResult = {
+      isValid: false,
+    };
     return result;
   }
 
@@ -79,5 +118,43 @@ export class AuditService {
     } else {
       throw Error('It is not apostille transaction');
     }
+  }
+
+  private async getTransactionFromPartial() {
+    const transactionRep = this.repositoryFactory.createTransactionRepository();
+    const tx = await transactionRep.getTransaction(this.txHash, TransactionGroup.Partial).toPromise();
+    if (tx instanceof AggregateTransaction) {
+      const coreInnerTx = tx.innerTransactions.find(t => t instanceof TransferTransaction && t.message.payload.startsWith('fe4e5459'));
+      if (coreInnerTx) {
+        return tx;
+      }
+      throw Error('It is not apostille transaction');
+    } else {
+      throw Error('It is not apostille transaction');
+    }
+  }
+
+  private getCoreTx(tx: AggregateTransaction) {
+    const coreTx = tx.innerTransactions.find(t => t instanceof TransferTransaction && t.message.payload.startsWith('fe4e5459'));
+    if (coreTx) {
+      return coreTx as TransferTransaction;
+    }
+    throw Error('It is not apostille transaction');
+  }
+
+  private getMultisigTx(tx: AggregateTransaction) {
+    const multisigTx = tx.innerTransactions.find(t => t instanceof MultisigAccountModificationTransaction);
+    if (multisigTx) {
+      return multisigTx as MultisigAccountModificationTransaction;
+    }
+    throw Error('It is not contain multisig tx');
+  }
+
+  private getCosignatories(tx: AggregateTransaction) {
+    const cosignatories: Address[] = [];
+    tx.cosignatures.forEach((c) => {
+      cosignatories.push(c.signer.address);
+    })
+    return cosignatories;
   }
 }
