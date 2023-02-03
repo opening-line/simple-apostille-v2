3ff8038f90e253a1afd7f1de0b95f9c934e54acd
rs-work-OL <65215644+rs-work-OL@users.noreply.github.com>
2023-02-03 17:42:14 +0900

    examples/ 初期入力値微調整

--- examples/AuditApostille.ts
+++ examples/AuditApostille.ts
@@ -2,7 +2,7 @@
 import { AuditService } from '../src/service';
 
 const data = 'Hello World';
-const txHash = '4ADCC18B2D72D8D40922D14A03B077C7D8BEC1FEE9934FE69E54DBBC3800E342';
+const txHash = '__INPUT_AUDITED_TRANSACTION_HASH__';
 const apiEndpoint = 'https://sym-test.opening-line.jp:3001';
 
 AuditService.audit(data, txHash, apiEndpoint).then((result) => {
--- examples/AuditWithPartialTx.ts
+++ examples/AuditWithPartialTx.ts
@@ -2,7 +2,7 @@
 import { AuditService } from "../src/service";
 
 const data = 'Hello World!';
-const txHash = '77CB1A251B028529C5AB07394D4DD5E769382C0E47FA4267B8694649ED75D003';
+const txHash = '__INPUT_AUDITED_TRANSACTION_HASH__';
 
 const apiEndpoint = 'https://sym-test.opening-line.jp:3001';
 
